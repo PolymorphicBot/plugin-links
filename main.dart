29e2a6c8a868607812c71cbdd4a0ddbced393bfb
Kenneth Endfinger <kaendfinger@gmail.com>
2014-09-20 17:02:10 -0400

    Update to new Plugin API

--- main.dart
+++ main.dart
@@ -1,45 +1,33 @@
 library links;
 
-import 'dart:io';
-import 'dart:convert';
-import 'dart:async';
-
 import 'package:http/http.dart' as http;
 import "package:irc/irc.dart";
 
 import 'package:html5lib/parser.dart' as html5 show parse;
-import 'package:html5lib/dom.dart';
 
 import 'package:polymorphic_bot/api.dart';
 
 BotConnector bot;
+EventManager eventManager;
 
 void main(List<String> args, port) {
   print("[Links] Loading Plugin");
   bot = new BotConnector(port);
 
-  bot.handleEvent((data) {
-    switch (data['event']) {
-      case "message":
-        handle_message(data);
-        break;
-    }
-  });
+  eventManager = bot.createEventManager();
+  
+  eventManager.on("message").listen(handleMessage);
 }
 
-final RegExp link_regex = new RegExp(r'\(?\b((http|https)://|www[.])[-A-Za-z0-9+&@#/%?=~_()|!:,.;]*[-A-Za-z0-9+&@#/%=~_()|]');
-
+final RegExp LINK_REGEX = new RegExp(r'\(?\b((http|https)://|www[.])[-A-Za-z0-9+&@#/%?=~_()|!:,.;]*[-A-Za-z0-9+&@#/%=~_()|]');
 final RegExp NO_SPECIAL_CHARS = new RegExp(r'''[^\w`~!@#$%^&*()\-_=+\[\]:'",<.>/?\\| ]''');
-
 final RegExp NO_MULTI_SPACES = new RegExp(r' {2,}');
+final RegExp YT_LINK = new RegExp(r'^.*(youtu.be/|v/|embed/|watch\?|youtube.com/user/[^#]*#([^/]*?/)*)\??v?=?([^#\&\?]*).*');
 
-
-final RegExp _yt_link_id = new RegExp(r'^.*(youtu.be/|v/|embed/|watch\?|youtube.com/user/[^#]*#([^/]*?/)*)\??v?=?([^#\&\?]*).*');
-
-void handle_message(data) {
+void handleMessage(data) {
   var msg = data['message'];
-  if (link_regex.hasMatch(msg)) {
-    for (var match in link_regex.allMatches(msg)) {
+  if (LINK_REGEX.hasMatch(msg)) {
+    for (var match in LINK_REGEX.allMatches(msg)) {
 
       var url = match.group(0);
 
@@ -47,12 +35,13 @@ void handle_message(data) {
         return;
       }
       
-      if (_yt_link_id.hasMatch(url)) return;
+      if (YT_LINK.hasMatch(url)) return;
 
       http.get(url).then((http.Response response) {
         if (response.statusCode != 200) {
           return;
         }
+        
         try {
           var document = html5.parse(response.body);
 
