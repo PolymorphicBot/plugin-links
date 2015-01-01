28e4f3e8b11b60c24a2f5ae99dedf421dd61bb54
Kenneth Endfinger <kaendfinger@gmail.com>
2015-01-01 13:04:51 -0500

    Update

--- main.dart
+++ main.dart
@@ -1,7 +1,9 @@
 library links;
 
+import 'dart:async';
+
 import 'package:http/http.dart' as http;
-import "package:irc/client.dart";
+import "package:irc/client.dart" as IRC;
 
 import 'package:html5lib/parser.dart' as html5 show parse;
 
@@ -13,7 +15,15 @@ void main(List<String> args, Plugin plugin) {
   print("[Links] Loading Plugin");
   bot = plugin.getBot();
   
-  plugin.on("message").listen(handleMessage);
+  plugin.addRemoteMethod("getLinkTitle", (call) {
+    getLinkTitle(call.getArgument("value")).then((title) {
+      call.reply(title);
+    }).catchError((e) {
+      call.reply(null);
+    });
+  });
+
+  bot.onMessage((event) => handleMessage(event));
 }
 
 final RegExp LINK_REGEX = new RegExp(r'\(?\b((http|https)://|www[.])[-A-Za-z0-9+&@#/%?=~_()|!:,.;]*[-A-Za-z0-9+&@#/%=~_()|]');
@@ -21,8 +31,8 @@ final RegExp NO_SPECIAL_CHARS = new RegExp(r'''[^\w`~!@#$%^&*()\-_=+\[\]:'",<.>/
 final RegExp NO_MULTI_SPACES = new RegExp(r' {2,}');
 final RegExp YT_LINK = new RegExp(r'^.*(youtu.be/|v/|embed/|watch\?|youtube.com/user/[^#]*#([^/]*?/)*)\??v?=?([^#\&\?]*).*');
 
-void handleMessage(data) {
-  var msg = data['message'];
+void handleMessage(MessageEvent event) {
+  var msg = event.message;
   if (LINK_REGEX.hasMatch(msg)) {
     for (var match in LINK_REGEX.allMatches(msg)) {
 
@@ -31,32 +41,37 @@ void handleMessage(data) {
       if (url.contains("github.com/")) {
         return;
       }
-      
+
       if (YT_LINK.hasMatch(url)) return;
-
-      http.get(url).then((http.Response response) {
-        if (response.statusCode != 200) {
-          return;
-        }
-        
-        try {
-          var document = html5.parse(response.body);
-
-          if (document == null) {
-            return;
-          }
-
-          var title = document.querySelector('title').text;
-
-          if (title == null || title.isEmpty) {
-            return;
-          }
-
-          title = title.replaceAll(NO_SPECIAL_CHARS, ' ').replaceAll(NO_MULTI_SPACES, ' ').trim();
-
-          bot.message(data['network'], data['target'], "[${Color.BLUE}Link Title${Color.RESET}] ${title}");
-        } catch (e) {}
+      
+      getLinkTitle(url).then((title) {
+        bot.message(event.network, event.target, "[${IRC.Color.BLUE}Link Title${IRC.Color.RESET}] ${title}");
       }).catchError((e) {});
     }
   }
+}
+
+Future<String> getLinkTitle(String url) {
+  return http.get(url).then((http.Response response) {
+    if (response.statusCode != 200) {
+      throw new Exception("FAIL");
+    }
+
+    try {
+      var document = html5.parse(response.body);
+
+      if (document == null) {
+        throw new Exception("FAIL");
+      }
+
+      var title = document.querySelector('title').text;
+
+      if (title == null || title.isEmpty) {
+        throw new Exception("FAIL");
+      }
+
+      title = title.replaceAll(NO_SPECIAL_CHARS, ' ').replaceAll(NO_MULTI_SPACES, ' ').trim();
+      return title;
+    } catch (e) {}
+  });
 }
